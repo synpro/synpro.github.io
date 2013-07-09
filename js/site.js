e70010eebb1f5c808424a6e9f5584ab6c66eb5ff
larsvegas <larsvegas@synpro.io>
2013-07-10 00:00:56 +0200

    retabbed site.js and added toggle for:q

--- js/site.js
+++ js/site.js
@@ -1,13 +1,16 @@
+jQuery(document).ready(function($) {
+  // scrolling
+  //$('#nav-main').scrollspy()
 
-	jQuery(document).ready(function($) {
+  // Localscrolling
+  $('#nav-main, .brand').localScroll();
+  $('#news, .container').localScroll();
 
-		    //$('#nav-main').scrollspy()
-		    
-		    // Localscrolling 
-    		$('#nav-main, .brand').localScroll();
-     		$('#news, .container').localScroll();
+  // disclaimer
+  $(".link").click(function() {
+    $(".disclaimerContent").toggle();
+    return true;
+  });
 
-	});
-	
-	
-
+  $('#news, .container').localScroll();
+});
